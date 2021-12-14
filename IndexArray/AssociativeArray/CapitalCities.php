aa3f493d440980e64d0bbfefb9c268ad1e69a8de
Afroj Satwilkar <88261856+AfrojSatwilkar@users.noreply.github.com>
2021-12-14 22:33:39 +0530

    Delete IndexArray/AssociativeArray directory

--- IndexArray/AssociativeArray/CapitalCities.php
+++
@@ -1,7 +0,0 @@
-<?php
-$country = array("USA" => "WASHINGTON DC", "NORWAY" => "OSLO", "ENGLAND" => "LONDON", "GERMANY" => "BERLIN");
-
-foreach ($country as $country => $capitalCities){
-    echo "Country = " . $country . ", Capital City = " . $capitalCities . "\n";
-}
-?>
